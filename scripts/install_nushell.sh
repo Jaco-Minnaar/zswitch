82763fffce1dcee6d438d4041f27fd9b289161ec
Jaco Minnaar <jminnaar@bscglobal.com>
2025-01-29 18:04:46 +0200

    fix(ci): make sure bin dir is created

--- scripts/install_nushell.sh
+++ scripts/install_nushell.sh
@@ -3,6 +3,7 @@
 wget https://github.com/nushell/nushell/releases/download/0.101.0/nu-0.101.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xvf nu-0.101.0-x86_64-unknown-linux-gnu.tar.gz
 
-cp nu-0.101.0-x86_64-unknown-linux-gnu/nu $HOME/.local/bin
+mkdir -p $HOME/.local/bin/
+cp nu-0.101.0-x86_64-unknown-linux-gnu/nu $HOME/.local/bin/nu
 rm -rf nu-0.101.0-x86_64-unknown-linux-gnu
 rm -f nu-0.101.0-x86_64-unknown-linux-gnu.tar.gz
